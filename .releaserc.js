0e6a0ac2a31ec91f45e77813447e88701af3ab11
Alexandrshy <alexandrshy@gmail.com>
2020-05-22 15:40:57 +0400

    fix: update-workflow

--- .releaserc.js
+++ .releaserc.js
@@ -1,9 +1,9 @@
 module.exports = {
     branch: 'master',
     plugins: [
+        '@semantic-release/commit-analyzer',
         '@semantic-release/changelog',
         ['@semantic-release/git', {assests: ['changelog.md']}],
-        '@semantic-release/commit-analyzer',
         '@semantic-release/npm',
         '@semantic-release/release-notes-generator',
     ],
