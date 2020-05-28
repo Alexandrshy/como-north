a248a70e50b59b9ee1960c92c2eb0c38b793c5a0
Alexandrshy <alexandrshy@gmail.com>
2020-05-29 01:49:28 +0400

    fix: added babel extension for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 import {terser} from 'rollup-plugin-terser';
 
 import pkg from './package.json';
@@ -19,6 +20,7 @@ export default {
         },
     ],
     plugins: [
+        babel(),
         typescript({
             rollupCommonJSResolveHack: true,
             clean: true,
